b00df3140cd718a0c9e1ee33697e0595d5e67e2e
maurofalcone <maurofalcone.95@gmail.com>
2019-02-03 23:38:15 -0300

    added register form

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import React, { Component } from "react"
 import { BrowserRouter as Router, Route } from "react-router-dom"
 import Navbar from "./components/layout/navbar/view"
 import Landing from "./components/layout/landing/view"
+import Register from "./components/auth/register/view"
 
 class App extends Component {
   render() {
     return (
       <Router>
         <div className="App">
-          <Navbar />
-          <Route exact path="/" component={Landing} />
+          <Navbar/>
+          <Route exact path="/" component={Landing}/>
+          <Route exact path="/register" component={Register}/>
         </div>
       </Router>
     )
