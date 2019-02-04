bc7883ace9a5902a455ca43608bde048a6c14780
maurofalcone <maurofalcone.95@gmail.com>
2019-02-03 23:47:50 -0300

    added login structure and style

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route } from "react-router-dom"
 import Navbar from "./components/layout/navbar/view"
 import Landing from "./components/layout/landing/view"
 import Register from "./components/auth/register/view"
+import Login from "./components/auth/login/view"
 
 class App extends Component {
   render() {
@@ -12,6 +13,7 @@ class App extends Component {
           <Navbar/>
           <Route exact path="/" component={Landing}/>
           <Route exact path="/register" component={Register}/>
+          <Route exact path="/login" component={Login}/>
         </div>
       </Router>
     )
