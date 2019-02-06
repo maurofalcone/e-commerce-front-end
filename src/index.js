8fee937615df8942f20c10a4d54fd6d32ab06f86
--global <--global>
2019-02-06 20:58:04 -0300

    api helpers to make requests and redux-thunk middleware were apply

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import rootReducer from './reducers'
 import App from './components/App'
+import { composeWithDevTools } from 'redux-devtools-extension/logOnlyInProduction'
+import thunk from 'redux-thunk'
 
-const store = createStore(rootReducer)
+export default function configureStore() {
+  const enhancer =  composeWithDevTools(
+    applyMiddleware(thunk)
+  )
+  return createStore(rootReducer, enhancer)
+}
+
+const store = configureStore()
 
 render(
   <Provider store={store}>
