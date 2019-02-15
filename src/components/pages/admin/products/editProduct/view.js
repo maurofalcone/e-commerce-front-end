4cb5b81a19427743a33607ba5e05c0416fc1b80b
--global <--global>
2019-02-15 14:05:37 -0300

    enabled to edit product

--- src/components/pages/admin/products/editProduct/view.js
+++ src/components/pages/admin/products/editProduct/view.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react"
+import { Redirect } from 'react-router-dom'
 import "./style.css"
 
 class EditProduct extends Component {
@@ -8,20 +9,21 @@ class EditProduct extends Component {
       id: '',
       name: '',
       description: '',
-      price: ''
+      price: '',
+      selectedFile:'',
+      redirect:false
     }
   }
 
-  componentDidMount() {
-    console.log(this.props.match.params.id);
+  componentWillMount() {
+    console.log(this.props.match.params.id)
     this.props.getProductById(this.props.match.params.id)
   }
 
-  componentWillReceiveProps() {
-    this.props.products.map(item => {
-        this.setState({ id:item.id,name:item.name,description:item.description,price:item.price })
-    })
-    console.log(this.state);
+  componentDidMount() {
+      this.props.products.map(item => {
+        return this.setState({ id:item.id,name:item.name,description:item.description,price:item.price })
+      })
   }
 
   onChangeName = (e) => {
@@ -36,62 +38,77 @@ class EditProduct extends Component {
     e.preventDefault()
     this.setState({description:e.target.value})
   }
-  onChangeImage = (e) => {
-    e.preventDefault()
-    this.setState({image:e.target.value})
-  }
 
   handleSave = (e) => {
     e.preventDefault()
-    const product = {
-      id:this.state.id,
-      name:this.state.name,
-      price:this.state.price,
-      description:this.state.description
+    if(this.state.selectedFile !== undefined) {
+      const fd = new FormData()
+      fd.append('image', this.state.selectedFile, this.state.selectedFile.name)
+      fd.append('description', this.state.description)
+      fd.append('name', this.state.name)
+      fd.append('price', this.state.price)
+      fd.append('id', this.props.match.params.id)
+      for (var p of fd) {
+        console.log('form data',p);
+      }
+      this.props.editProduct(fd)
+      this.setState({redirect:true})
     }
-    this.props.editProduct(product)
-    this.setState({name:'',image:'',price:'',description:''})
+    else {
+      console.log('error, need to load a file')
+    }
+  }
+
+  redirect() {
+      if(this.state.redirect === true)
+        return <Redirect to="/admin/products"/>
   }
 
   handleCancel = (e) => {
     e.preventDefault()
-    this.setState({name:'',image:'',price:'',description:''})
-    //move to products because using Link does not work correctly
-    this.props.history.push('/admin/products')
+    this.setState({name:'',selectedFile:'',price:'',description:''})
+    this.setState({redirect:true})
+  }
+
+  handleSelectedFile = (e) => {
+    this.setState({selectedFile:e.target.files[0]})
   }
 
   render() {
     if(this.props.isLoading === false) {
-      console.log('product encontrado', this.props.products)
-      return (
-            <div className="container">
-                <form noValidate onSubmit={this.onSubmit}>
-                  <div className="input-field col s5">
-                    <input onChange={this.onChangeName} value={this.state.name} id="editProductName" type="text"/>
-                    <label className="active" htmlFor="editProductName">Name</label>
-                  </div>
-                  <div className="input-field col s5">
-                    <input onChange={this.onChangePrice} value={this.state.price} id="editProductPrice" type="text"/>
-                    <label className="active" htmlFor="editProductPrice">Price</label>
-                  </div>
-                  <div className="input-field col s5">
-                    <textarea onChange={this.onChangeDescription} value={this.state.description} id="editProductDescription" type="text"/>
-                    <label className="active" htmlFor="editProductDescription">Description</label>
-                  </div>
-                  <button onClick={ this.handleSave } id="editProductSave" className="btn btn-large waves-effect waves-light hoverable blue"><span className="white-text">Save</span></button>
-                  <button onClick={ this.handleCancel } id="editProductCancel" className="btn btn-large waves-effect white hoverable black-text">Cancel</button>
-                </form>
-              </div>
-            )
-          }
-          else {
-            return (
+        return (
               <div className="container">
-                <h6>Loading ...</h6>
-              </div>
-            )
+                  <form noValidate onSubmit={this.onSubmit}>
+                    <div className="input-field col s5">
+                      <input onChange={this.onChangeName} value={this.state.name} id="editProductName" type="text"/>
+                      <label className="active" htmlFor="editProductName">Name</label>
+                    </div>
+                    <div className="input-field col s5">
+                      <input type="file" id="editProductImage" onChange={this.handleSelectedFile}/>
+                    </div>
+                    <div className="input-field col s5">
+                      <input onClick={console.log(this.state)} onChange={this.onChangePrice} value={this.state.price} id="editProductPrice" type="text"/>
+                      <label className="active" htmlFor="editProductPrice">Price</label>
+                    </div>
+                    <div className="input-field col s5">
+                      <textarea onChange={this.onChangeDescription} value={this.state.description} id="editProductDescription" type="text"/>
+                      <label className="active" htmlFor="editProductDescription">Description</label>
+                    </div>
+                    <button onClick={ this.handleSave } id="editProductSave" className="btn btn-large waves-effect waves-light hoverable blue"><span className="white-text">Save</span></button>
+                    <button onClick={ this.handleCancel } id="editProductCancel" className="btn btn-large waves-effect white hoverable black-text">Cancel</button>
+                  </form>
+                  {this.redirect()}
+                </div>
+              )
+            }
+            else {
+              return (
+                <div className="container">
+                  <h6>Loading ...</h6>
+                </div>
+              )
+            }
           }
-        }
       }
 
 
