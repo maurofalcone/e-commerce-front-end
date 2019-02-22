99abb4864f9dfbf9fddd0bbd5ca4cd7cbc661634
--global <--global>
2019-02-22 18:00:45 -0300

    fixed layout and new style

--- src/components/utils/warning/view.js
+++ src/components/utils/warning/view.js
@@ -1,20 +1,25 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 import './style.css'
 
 class Warning extends Component {
   render () {
     return (
-      <div className="container valign-wrapper">
-        <div className="row">
-          <div className="col s3">
-          <br />
-          <br />
-          <i className="material-icons medium">info</i>
-          </div>
-          <div className="col s9">
-            <blockquote>
-              <h1>Access denied</h1>
-            </blockquote>
+      <div>
+        <div className="container valign-wrapper">
+          <div className="row">
+            <div className="col s2">
+              <br/>
+              <br/>
+              <br/>
+              <br/>
+              <i className="material-icons medium">security</i>
+            </div>
+            <div className="col s10">
+              <blockquote>
+                <h1>Access denied</h1>
+              </blockquote>
+            </div>
           </div>
         </div>
       </div>
