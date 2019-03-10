5817eec5c0f9dd77f89b30c9dbb6de24b14c634e
Mauro Falcone <maurofalcone@Mauro-Falcone.local>
2019-03-10 12:50:38 -0300

    display login errors

--- src/components/auth/login/view.js
+++ src/components/auth/login/view.js
@@ -2,6 +2,7 @@ import React, { Component } from "react"
 import ReactLoading from 'react-loading'
 import { Link, Redirect } from "react-router-dom"
 import isEmpty from 'is-empty'
+
 const token = localStorage.getItem("jwtToken")
 
 class Login extends Component {
@@ -72,42 +73,45 @@ class Login extends Component {
       }
       else {
         if(!token) {
-          return (
-            <div className="container">
-              <div id="loginRow" className="row">
-                <div className="col s8 offset-s2">
-                  <Link to="/" className="btn-flat waves-effect"><i className="material-icons left">keyboard_backspace</i>Back to home</Link>
-                  <div className="col s12">
-                    <h4>
-                      <b>Login</b> below
-                    </h4>
-                    <p className="grey-text text-darken-1">
-                      Don't have an account? <Link to="/register">Register</Link>
-                    </p>
-                  </div>
-                  <form>
-                    <div className="input-field col s12">
-                      <input onChange={this.onChange} value={this.state.email} id="email" type="email"/>
-                      <label htmlFor="email">Email</label>
-                    </div>
-                    <div>
-                      <label className="red-text">{this.state.errorEmail}</label>
-                    </div>
-                    <div className="input-field col s12">
-                      <input onChange={this.onChange} value={this.state.password} id="password" type="password"/>
-                      <label htmlFor="password">Password</label>
-                    </div>
-                    <div>
-                      <label className="red-text">{this.state.errorPassword}</label>
-                    </div>
+            return (
+              <div className="container">
+                <div id="loginRow" className="row">
+                  <div className="col s8 offset-s2">
+                    <Link to="/" className="btn-flat waves-effect"><i className="material-icons left">keyboard_backspace</i>Back to home</Link>
                     <div className="col s12">
-                      <button id="loginBtn" onClick={this.onSubmit} type="button" className="btn btn-large waves-effect waves-light hoverable blue accent-3"> Login </button>
+                      <h4>
+                        <b>Login</b> below
+                      </h4>
+                      <p className="grey-text text-darken-1">
+                        Don't have an account? <Link to="/register">Register</Link>
+                      </p>
                     </div>
-                  </form>
+                    <form>
+                      <div className="input-field col s12">
+                        <input onChange={this.onChange} value={this.state.email} id="email" type="email"/>
+                        <label htmlFor="email">Email</label>
+                      </div>
+                      <div>
+                        <label className="red-text">{this.state.errorEmail}</label>
+                      </div>
+                      <div className="input-field col s12">
+                        <input onChange={this.onChange} value={this.state.password} id="password" type="password"/>
+                        <label htmlFor="password">Password</label>
+                      </div>
+                      <div>
+                        <label className="red-text">{this.state.errorPassword}</label>
+                      </div>
+                      <div>
+                        <label className="red-text">{this.props.error}</label>
+                      </div>
+                      <div className="col s12">
+                        <button id="loginBtn" onClick={this.onSubmit} type="button" className="btn btn-large waves-effect waves-light hoverable blue accent-3"> Login </button>
+                      </div>
+                    </form>
+                  </div>
                 </div>
               </div>
-            </div>
-          )
+            )
         }
         else {
           return (
