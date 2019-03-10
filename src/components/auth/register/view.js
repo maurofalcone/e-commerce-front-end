66cae8903edd8692ea0c9a46eaea0c0dbb0e8adb
Mauro Falcone <maurofalcone@Mauro-Falcone.local>
2019-03-09 22:03:11 -0300

    register validation and style

--- src/components/auth/register/view.js
+++ src/components/auth/register/view.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react"
 import { Link } from "react-router-dom"
+import isEmpty from 'is-empty'
+import './style.css'
 
 const token = localStorage.getItem("jwtToken")
 
@@ -11,26 +13,69 @@ class Register extends Component {
       email: "",
       password: "",
       password2: "",
-      errors: {}
+      errorName:'',
+      errorEmail:'',
+      errorPassword:'',
+      errorPassword2:''
     }
   }
 
-onChange = e => {
-    this.setState({ [e.target.id]: e.target.value })
+  emailIsValid = (email) => {
+    return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)
   }
 
-onSubmit = e => {
-    e.preventDefault()
-    const newUser = {
-      name: this.state.name,
-      email: this.state.email,
-      password: this.state.password,
-      password2: this.state.password2
+  validate = () => {
+    if(isEmpty(this.state.name)) {
+      this.setState({ errorName:'please enter a name' })
+      return false
     }
-    console.log(newUser)
+    let email = this.emailIsValid(this.state.email)
+    if(!email) {
+      this.setState({ errorEmail:'invalid email' })
+      return false
+    }
+    if(isEmpty(this.state.email)) {
+      this.setState({ errorEmail:'invalid email' })
+      return false
+    }
+    if(isEmpty(this.state.password) || (this.state.password.length < 6)) {
+      this.setState({ errorPassword:'invalid password' })
+      return false
+    }
+    if(isEmpty(this.state.password2) || (this.state.password2.length < 6)) {
+      this.setState({ errorPassword2:'invalid password' })
+      return false
+    }
+      return true
   }
+ 
+  clearForm = () => {
+    this.setState({ password:'',email:'' })
+  }
+
+  clearError = () => {
+    this.setState({ errorName:'',errorEmail:'', errorPassword:'', errorPassword2:'' })
+  }
+
+  onChange = e => {
+     this.setState({ [e.target.id]: e.target.value }, () => { this.clearError() })
+  }
+
+  onSubmit = e => {
+      e.preventDefault()
+      if(this.validate()) {
+        const newUser = {
+          name: this.state.name,
+          email: this.state.email,
+          password: this.state.password,
+          password2: this.state.password2
+      }
+      this.clearForm()
+      this.props.registerUser(newUser)
+    }
+  }
+
   render() {
-    const { errors } = this.state
     if(!token) {
       return (
           <div className="container">
@@ -39,7 +84,7 @@ onSubmit = e => {
                 <Link to="/" className="btn-flat waves-effect">
                   <i className="material-icons left">keyboard_backspace</i> Back to home
                 </Link>
-                <div className="col s12" style={{ paddingLeft: "11.250px" }}>
+                <div className="col s12 registerColumn">
                   <h4>
                     <b>Register</b> below
                   </h4>
@@ -49,23 +94,35 @@ onSubmit = e => {
                 </div>
                 <form noValidate onSubmit={this.onSubmit}>
                   <div className="input-field col s12">
-                    <input onChange={this.onChange} value={this.state.name} error={errors.name} id="name" type="text"/>
+                    <input onChange={this.onChange} value={this.state.name} id="name" type="text"/>
                     <label htmlFor="name">Name</label>
                   </div>
+                  <div>
+                    <label className="red-text">{this.state.errorName}</label>
+                  </div>
                   <div className="input-field col s12">
-                    <input onChange={this.onChange} value={this.state.email} error={errors.email} id="email" type="email"/>
+                    <input onChange={this.onChange} value={this.state.email} id="email" type="email"/>
                     <label htmlFor="email">Email</label>
                   </div>
+                  <div>
+                    <label className="red-text">{this.state.errorEmail}</label>
+                  </div>
                   <div className="input-field col s12">
-                    <input onChange={this.onChange} value={this.state.password} error={errors.password} id="password" type="password"/>
+                    <input onChange={this.onChange} value={this.state.password} id="password" type="password"/>
                     <label htmlFor="password">Password</label>
                   </div>
+                  <div>
+                    <label className="red-text">{this.state.errorPassword}</label>
+                  </div>
                   <div className="input-field col s12">
-                    <input onChange={this.onChange} value={this.state.password2} error={errors.password2} id="password2" type="password"/>
+                    <input onChange={this.onChange} value={this.state.password2} id="password2" type="password"/>
                     <label htmlFor="password2">Confirm Password</label>
                   </div>
-                  <div className="col s12" style={{ paddingLeft: "11.250px" }}>
-                    <button style={{ width: "150px", borderRadius: "3px", letterSpacing: "1.5px", marginTop: "1rem" }} type="submit" className="btn btn-large waves-effect waves-light hoverable blue accent-3"> Sign up </button>
+                  <div>
+                    <label className="red-text">{this.state.errorPassword2}</label>
+                  </div>
+                  <div className="col s12 registerColumn">
+                    <button type="submit" className="btn btn-large waves-effect waves-light hoverable blue accent-3"> Sign up </button>
                   </div>
                 </form>
               </div>
